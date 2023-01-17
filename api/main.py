2737a94db26bae97dcb5d621f404dceee3095388
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-01-17 21:46:31 +0300

    init project

---
+++ api/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+# from db.base import database
+# from routers import router
+
+app = FastAPI()
+# app.include_router(router)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
