9db7876a048f59d4e50d878eff4af9c6cb80352d
Andreyfrolov44 <andre.frolov44@gmail.com>
2023-02-02 17:20:49 +0300

    fix asyncpg

--- api/db/base.py
+++ api/db/base.py
@@ -4,5 +4,4 @@ import sqlalchemy
 from core.config import DATABASE_URL
 
 database = databases.Database(DATABASE_URL)
-engine = sqlalchemy.create_engine(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
